d9402db3000dca45cc507d97fc5a0b85805da4f0
Lucia Draesner <lucia.draesner@code.berlin>
2026-09-23 21:22:55 +0200

    Improve About page design

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
-// Store a reference to the <h1> in a variable
-const myHeading = document.querySelector("h1");
-// Update the text content of the <h1>
-myHeading.textContent = "Alex Draesner: Portfolio";
-
-
 const myImage = document.querySelector("img");
 
 myImage.addEventListener("click", () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "3d-pager.png") {
-    myImage.setAttribute("src", "website-code.png");
+
+  if (mySrc === "/3d-pager.png") {
+    myImage.setAttribute("src", "/website-code.png");
   } else {
-    myImage.setAttribute("src", "3d-pager.png");
+    myImage.setAttribute("src", "/3d-pager.png");
   }
 });
-
